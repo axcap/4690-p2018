4523400844acb5ff4894d56ae5f79e792e6ee68c
akhsarbg <akhsarbg@uio.no>
2018-04-25 11:13:25 +0200

    Moved mnist dataset to dataset folder (downloads dynamicly, if not present)

--- src/nn_mnist.py
+++ src/nn_mnist.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
     sess = tf.InteractiveSession()
 
-    path = "../res/model/mnist_demo"
+    path = "res/model/mnist_demo"
     saver = tf.train.Saver()
     if Path(path+".index").is_file():
         saver.restore(sess, path)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     else:
         print("Not trained")
         
-        mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)    
+        mnist = input_data.read_data_sets("res/datasets/MNIST/", one_hot=True)    
         tf.global_variables_initializer().run()
         np.set_printoptions(linewidth=9999999)
         for _ in range(1000):
@@ -47,18 +47,18 @@ if __name__ == "__main__":
             plt.show()
             '''
             sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-            correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            print("Accuracity: "+str(sess.run(accuracy,
-                                              feed_dict={x: mnist.test.images, y_: mnist.test.labels})))
-            tf.logging.set_verbosity(old_v)
+        correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print("Accuracity: "+str(sess.run(accuracy,
+                                        feed_dict={x: mnist.test.images, y_: mnist.test.labels})))
+        tf.logging.set_verbosity(old_v)
 
         save_path = saver.save(sess, path)
         print("Model saved in path: %s" % save_path)
 
 
 
-    img = cv2.imread('../res/images/1.png');
+    img = cv2.imread('res/images/1.png');
     img_small = cv2.resize(img, (28,28))
     img_grey = cv2.cvtColor(255-img_small, cv2.COLOR_BGR2GRAY)
     demo = np.reshape(img_grey, (1, -1))
