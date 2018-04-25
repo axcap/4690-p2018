e603e02002f302d55b9987a99be1b29ef68f0f35
akhsarbg <akhsarbg@uio.no>
2018-04-25 12:38:09 +0200

    mnist_gui now uses classes version of NN

--- src/nn_mnist.py
+++ src/nn_mnist.py
@@ -56,20 +56,21 @@ class NN_MNIST:
     def forward(self, data):
         return self.sess.run(self.y, feed_dict={self.x: data})
 
-np.set_printoptions(linewidth=9999999)
-
-#Load training data 
-mnist = input_data.read_data_sets("res/datasets/MNIST/", one_hot=True)
-nn = NN_MNIST()
-
-nn.train(mnist, force_retrain=True)
-
-img = cv2.imread('res/images/1.png');
-img_small = cv2.resize(img, (28,28))
-img_grey = cv2.cvtColor(255-img_small, cv2.COLOR_BGR2GRAY)
-print(img_grey)
-
-data = (1/256)*img_grey
-data = np.reshape(data, (1, 28*28))
-r = nn.forward(data)
-print(np.argmax(r))
+if __name__ == "__main__":
+    np.set_printoptions(linewidth=9999999)
+    
+    #Load training data 
+    mnist = input_data.read_data_sets("res/datasets/MNIST/", one_hot=True)
+    nn = NN_MNIST()
+    
+    nn.train(mnist, force_retrain=True)
+    
+    img = cv2.imread('res/images/1.png');
+    img_small = cv2.resize(img, (28,28))
+    img_grey = cv2.cvtColor(255-img_small, cv2.COLOR_BGR2GRAY)
+    print(img_grey)
+    
+    data = (1/256)*img_grey
+    data = np.reshape(data, (1, 28*28))
+    r = nn.forward(data)
+    print(np.argmax(r))
