23bd94f4888bc6ed4a4a5a41353ff112b0ac7f2d
Key <keylung94@gmai.com>
2018-04-26 17:51:55 +0200

    started with text region segmentation

---
+++ src/Keys Tests/text_segmentation.py
@@ -0,0 +1,49 @@
+import cv2 
+import numpy as np 
+from skimage.filters.rank import median
+from skimage.morphology import disk
+import test_find_contour as test 
+
+IMAGE_PATH = '../../res/images/'
+
+def detect_text(image):
+    """
+        input: image
+        return: list of text segments images
+    """
+    edge_img = cv2.Canny(image,100,200)
+     
+    blur_image = cv2.medianBlur(edge_img, 5)
+    kernel = np.ones((5,5),np.uint8)
+
+    # number of iteration need to be bether impremented, currently hardcoded
+    morph_img = cv2.dilate(blur_image, kernel, iterations=10)
+
+    img, contours, hier = cv2.findContours(morph_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    text_area_found = []
+
+    for c in contours:
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(c)
+
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 1)
+        text_area_found.append(image[y+1:y+h, x+1:x+w]) # +1 to remove edge
+  
+    cv2.imshow("Image segment", morph_img) 
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    return text_area_found
+
+def main():
+    image_filename = 'text_example2.png'
+    image = cv2.imread(IMAGE_PATH+image_filename,0)
+    text_area = detect_text(image)
+
+    for i in range(len(text_area)):
+        cv2.imshow("text", text_area[i]) 
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
