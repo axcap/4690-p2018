6b05d124cb5b51f51acda8b1cfa27158c1733580
Key <keylung94@gmai.com>
2018-05-15 17:04:08 +0200

    finish for today

--- src/Keys Tests/text_detection/openCV_text_detection.py
+++ src/Keys Tests/text_detection/openCV_text_detection.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import math
 import os
@@ -6,7 +6,7 @@ import sys
 
 def detect_text(image):
 
-    channels = cv.text.computeNMChannels(image)
+    channels = cv2.text.computeNMChannels(image)
     vis = image.copy()
 
     cn = len(channels)-1
@@ -15,30 +15,30 @@ def detect_text(image):
         channels.append((255-channels[c]))
 
     for channel in channels:
-        erc1 = cv.text.loadClassifierNM1('trained_classifierNM1.xml')
-        er1 = cv.text.createERFilterNM1(erc1,16,0.00010,0.40,0.2,True,0.1)
+        erc1 = cv2.text.loadClassifierNM1('trained_classifierNM1.xml')
+        er1 = cv2.text.createERFilterNM1(erc1,16,0.00010,0.40,0.2,True,0.1)
 
-        erc2 = cv.text.loadClassifierNM2('trained_classifierNM2.xml')
-        er2 = cv.text.createERFilterNM2(erc2,0.5)
+        erc2 = cv2.text.loadClassifierNM2('trained_classifierNM2.xml')
+        er2 = cv2.text.createERFilterNM2(erc2,0.5)
 
-        regions = cv.text.detectRegions(channel,er1,er2)
+        regions = cv2.text.detectRegions(channel,er1,er2)
 
         # print(regions)
         if regions == []:
             continue
             
-        rects = cv.text.erGrouping(image, channel, [r.tolist() for r in regions])
+        rects = cv2.text.erGrouping(image, channel, [r.tolist() for r in regions])
 
         for r in range(0,np.shape(rects)[0]):
             rect = rects[r]
-            cv.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (0, 0, 0), 2)
-            cv.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (255, 255, 255), 1)
+            cv2.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (0, 0, 0), 2)
+            cv2.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (255, 255, 255), 1)
 
     #Visualization
-    cv.imshow("Text detection result", vis)
-    cv.waitKey(0)
+    cv2.imshow("Text detection result", vis)
+    cv2.waitKey(0)
 
-    cv.imwrite('detect_text.jpg', image)
+    cv2.imwrite('detect_text.jpg', image)
 
 
 def main():
@@ -47,7 +47,7 @@ def main():
     IMAGE_PATH = '../../res/images/'
     image_filename = 'text_skew.png'
 
-    image = cv.imread(IMAGE_PATH+image_filename)
+    image = cv2.imread(IMAGE_PATH+image_filename)
 
 
     detect_text(image)
