091572d59ba887d672e4cec2fd406b3684160ab5
Akhsarbek Gozoev <akhsarbg@uio.no>
2018-05-15 14:02:53 +0200

    New network topology (2 hidden layers) implementet, nn.forward() optimized

--- src/show_mnist_digit.py
+++ src/show_mnist_digit.py
@@ -9,14 +9,23 @@ np.set_printoptions(linewidth=9999999)
 if(len(sys.argv) < 2):
     print("\n\nUsage: python %s N" % sys.argv[0])
     exit(1)
-            
-digit = int(sys.argv[1])
 
-mnist = input_data.read_data_sets("res/datasets/MNIST/", one_hot=True)
-labels = np.argmax(mnist.train.labels, axis=1)
+digit = int(sys.argv[1])
+database = input_data.read_data_sets("res/datasets/MNIST/", one_hot=True, validation_size=10)
+for i in range(len(database.train.images)):
+    img = np.reshape((database.train.images[i]*255).astype(int), (28, 28))
+    print(img)
+    print(database.train.labels[i])
+    input()
+
+
+'''
+labels = np.argmax(database.train.labels, axis=1)
 arr = np.where(labels == digit)[0]
+print("Len: ", len(arr))
 
 for i in range(len(arr)):
-    img = np.reshape((mnist.train.images[arr[i]]*255).astype(np.int), (28, 28))
+    img = np.reshape((database.train.images[arr[i]]*255).astype(np.int), (28, 28))
     print(img)
     input()
+'''
