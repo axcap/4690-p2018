695dbf786e232da299981f29bac7aa2d467a30e1
Akhsarbek Gozoev <akhsarbg@uio.no>
2018-05-16 14:10:24 +0200

    Added show_lines/symbols functions to utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def histogramProjection(img, direction = 'vertical'):
     n = img.shape[1] if direction is 'vertical' else img.shape[0]
@@ -14,6 +15,26 @@ def histogramProjection(img, direction = 'vertical'):
         summ = np.sum(col >= 100)
         sumCols.append(summ)
     return sumCols
+
+#Returns image with rectangles around around each line
+def show_lines(img):
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    linesHist = find_lines(img)
+    lines = segment_lines(img, linesHist)
+    y,x = img.shape[:2]
+    for l in lines:
+        cv2.rectangle(img, (0, l[0]), (x, l[1]), color=(255,0,0), thickness = 1)
+    return img
+
+#Returns image with rectangles around around each symbol in input line image
+def show_symbols(img):
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    linesHist = find_symbol(img)
+    lines = segment_symbols(img, linesHist)
+    y,x = img.shape[:2]
+    for l in lines:
+        cv2.rectangle(img, (l[0], 0), (l[1], y), color=(255,0,0), thickness = 1)
+    return img
 
 
 def find_lines(img):
@@ -38,7 +59,8 @@ def segment_hist(img, hist):
         if hist[i] != 0:
             p1 = i
             for j in range(i, len(hist)):
-                if hist[j] == 0:
+                #if 'space between hists found' or end reached
+                if hist[j] == 0 or j == len(hist)-1:
                     skip = j-i
                     p2 = j
                     points.append((p1, p2))
