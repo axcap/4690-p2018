b4b0262148777a72237dd8b85b4e3213992ecac6
Key <keylung94@gmai.com>
2018-05-18 17:04:43 +0200

    added addition file to text detection

--- src/Keys Tests/text_detection/simple_text_detection.py
+++ src/Keys Tests/text_detection/simple_text_detection.py
@@ -50,10 +50,5 @@ def main():
     image = cv2.imread(IMAGE_PATH+image_filename,0)
     text_area = detect_text(image)
 
-    # for i in range(len(text_area)):
-    #     cv2.imshow("text", text_area[i]) 
-    #     cv2.waitKey()
-    #     cv2.destroyAllWindows()
-
 if __name__ == '__main__':
     main()
