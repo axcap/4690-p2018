873758875969473e0eb5fba32e7077b9a9727f63
Akhsarbek Gozoev <akhsarbg@uio.no>
2018-05-19 22:52:16 +0200

    font fixing to match mnist scale

--- src/show_mnist_digit.py
+++ src/show_mnist_digit.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
+
 from tensorflow.examples.tutorials.mnist import input_data
 from matplotlib.widgets import Button
 from matplotlib import pyplot as plt
@@ -10,23 +14,25 @@ if(len(sys.argv) < 2):
     print("\n\nUsage: python %s N" % sys.argv[0])
     exit(1)
 
-digit = int(sys.argv[1])
-database = input_data.read_data_sets("res/datasets/ROTMNIST/", one_hot=True, validation_size=10)
-
-for i in range(len(database.train.images)):
-    img = np.reshape((database.train.images[i]*255).astype(int), (28, 28))
-    print(img)
-    print(database.train.labels[i])
-    input()
+path  = sys.argv[1]
+digit = int(sys.argv[2])
+dataset = input_data.read_data_sets(path)
 
 
-'''
-labels = np.argmax(database.train.labels, axis=1)
-arr = np.where(labels == digit)[0]
-print("Len: ", len(arr))
+collection = np.where(dataset.train.labels == digit)
 
-for i in range(len(arr)):
-    img = np.reshape((database.train.images[arr[i]]*255).astype(np.int), (28, 28))
-    print(img)
-    input()
-'''
+num = 8
+idx = 0
+while True:
+  fig = plt.figure()
+  for y in range(num):
+    for x in range(num):
+      img = dataset.train.images[collection[0][idx]]
+      img = (np.reshape(img, (28,28))*255).astype(int)
+      ax = fig.add_subplot(num,num, y*num+x +1)
+      print(img, "\n")
+      #ax.imshow(img, cmap='gray')
+      ax.imshow(img)
+      idx += 1
+
+  plt.show()
