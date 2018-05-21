4f9f14df149cd26a245bc5b5b97ff8de62487ff4
Akhsarbek Gozoev <akhsarbg@uio.no>
2018-05-21 17:31:45 +0200

    Working digit/letter recognizer implemented (>80% accuracity)

---
+++ src/akhsabrg_tests/.#nn_test.py
@@ -0,0 +1,1 @@
+akhsarbg@gideon.ifi.uio.no.4140:1526303981
--- src/main.py
+++ src/main.py
@@ -3,6 +3,26 @@ import segment as segment
 import utils as utils
 import cv2
 
+def extractText(img):
+    linesHist = utils.find_lines(seg)
+    lines = utils.segment_lines(seg, linesHist)
+    for i, l in enumerate(lines):
+        single_line = seg[l[0]:l[1], 0:x]
+        symbolHist = utils.find_symbol(single_line)
+        symbols    = utils.segment_symbols(seg, symbolHist)
+        for s in symbols:
+            single_symbol = seg[l[0]:l[1], s[0]:s[1]]
+            single_symbol = cv2.resize(single_symbol, (28,28))
+            single_symbol[single_symbol > 30] = 255
+            single_symbol[single_symbol <= 30] = 0
+            #utils.imshow("Single symbol", single_symbol)
+
+            digit = nn.forward(single_symbol)
+            print(digit, end=" ")
+
+      print("")
+
+
 if __name__ == "__main__":
 
   nn = classifier.Classifier()
@@ -22,24 +42,8 @@ if __name__ == "__main__":
     for contour in contours[::-1]:
       seg = utils.extractContour(binary, contour)
       #utils.imshow("Segment", seg)
+      extractContourText(seg)
 
-      linesHist = utils.find_lines(seg)
-      lines = utils.segment_lines(seg, linesHist)
-      for i, l in enumerate(lines):
-        single_line = seg[l[0]:l[1], 0:x]
-        symbolHist = utils.find_symbol(single_line)
-        symbols    = utils.segment_symbols(seg, symbolHist)
-        for s in symbols:
-            single_symbol = seg[l[0]:l[1], s[0]:s[1]]
-            single_symbol = cv2.resize(single_symbol, (28,28))
-            single_symbol[single_symbol > 30] = 255
-            single_symbol[single_symbol <= 30] = 0
-            #utils.imshow("Single symbol", single_symbol)
-
-            digit = nn.forward(single_symbol)
-            print(digit, end=" ")
-
-      print("")
 
     print("\n")
     if input("Press 'Enter' to preceed to next image: ") == 'q':
