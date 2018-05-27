6a260b8e841632daca923e9de30bdf8062002a6e
Akhsarbek Gozoev <akhsarbg@uio.no>
2018-05-27 10:48:13 +0200

    Added visualizer code

--- src/akhsabrg_tests/cnn_mnist.py
+++ src/akhsabrg_tests/cnn_mnist.py
@@ -20,6 +20,8 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+import cv2
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -43,6 +45,7 @@ def cnn_model_fn(features, labels, mode):
       inputs=input_layer,
       filters=32,
       kernel_size=[5, 5],
+      name = "conv1",
       padding="same",
       activation=tf.nn.relu)
 
@@ -61,6 +64,7 @@ def cnn_model_fn(features, labels, mode):
       inputs=pool1,
       filters=64,
       kernel_size=[5, 5],
+      name = "conv2",
       padding="same",
       activation=tf.nn.relu)
 
@@ -103,6 +107,14 @@ def cnn_model_fn(features, labels, mode):
   # Calculate Loss (for both TRAIN and EVAL modes)
   loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
+  predicted_classes = tf.argmax(logits, 1)
+  # Compute evaluation metrics.
+  accuracy = tf.metrics.accuracy(labels=labels,
+                                 predictions=predictions['classes'],
+                                 name='acc_op')
+  metrics = {'accuracy': accuracy}
+  tf.summary.scalar('accuracy', accuracy[1])
+    
   # Configure the Training Op (for TRAIN mode)
   if mode == tf.estimator.ModeKeys.TRAIN:
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
@@ -131,7 +143,7 @@ def main(unused_argv):
 
   # Create the Estimator
   mnist_classifier = tf.estimator.Estimator(
-      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+      model_fn=cnn_model_fn, model_dir="res/model/visual_cnn")
 
   # Set up logging for predictions
   # Log the values in the "Softmax" tensor with label "probabilities"
@@ -146,10 +158,9 @@ def main(unused_argv):
       batch_size=100,
       num_epochs=None,
       shuffle=True)
-  mnist_classifier.train(
-      input_fn=train_input_fn,
-      steps=20000,
-      hooks=[logging_hook])
+
+
+  mnist_classifier.train(input_fn=train_input_fn,steps=1000)#, hooks=[logging_hook])
 
   # Evaluate the model and print results
   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -169,6 +180,19 @@ def main(unused_argv):
   eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
   print(eval_results)
 
+  img = train_data[-3]
+
+  weights = mnist_classifier.get_variable_value('conv1/kernel')
+  kernel = np.reshape(weights[:,:,:,0], (5,5))
+  img = cv2.filter2D(img, -1, kernel)
+  plt.imshow(np.reshape(img, (28,28)))
+  plt.show()
+
+  weights = mnist_classifier.get_variable_value('conv2/kernel')
+  kernel = np.reshape(weights[:,:,0,0], (5,5))
+  img = cv2.filter2D(img, -1, kernel)
+  plt.imshow(np.reshape(img, (28,28)))
+  plt.show()
 
 
 if __name__ == "__main__":
